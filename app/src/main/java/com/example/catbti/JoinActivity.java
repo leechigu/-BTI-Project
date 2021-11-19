38b977ca62100bd0cd409a807ae6dcbdc1c7abe6
soohwan <newpoo@naver.com>
2021-11-19 16:10:49 +0900

    hehe

---
+++ app/src/main/java/com/example/catbti/JoinActivity.java
@@ -0,0 +1,34 @@
+package com.example.catbti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class JoinActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/catbti/MainActivity.java
+++ app/src/main/java/com/example/catbti/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.catbti;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    GoogleSignInClient mGoogle;
+    public static int RC_SIGN_IN = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Button mbtiJobButton = findViewById(R.id.mbtiJobButton);
 
-        Button explainMbti = (Button)findViewById(R.id.mbtiButton);
+        Button explainMbti = (Button) findViewById(R.id.mbtiButton);
         explainMbti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,17 +42,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button boardList = (Button)findViewById(R.id.boardButton);
+
+
+        Button boardList = (Button) findViewById(R.id.boardButton);
         boardList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent boardListIntent = new Intent(MainActivity.this, BoardList.class);
+                Intent boardListIntent = new Intent(MainActivity.this, ListActivity.class);
                 MainActivity.this.startActivity(boardListIntent);
 
             }
         });
 
-        Button testMbti = (Button)findViewById(R.id.testButton);
+        Button testMbti = (Button) findViewById(R.id.testButton);
         testMbti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +72,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogle = GoogleSignIn.getClient(MainActivity.this, gso);
+
+        Button logout_btn_google;
+
+        logout_btn_google = (Button) findViewById(R.id.logout_btn_google);
+
+        logout_btn_google.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mGoogle.signOut();
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
 
 }
