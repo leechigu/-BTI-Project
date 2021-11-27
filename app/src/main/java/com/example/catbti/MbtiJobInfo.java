ec5225401aa377530a5793bc72f8e756c2b4bb0f
Hyewon419 <hijjoy@naver.com>
2021-11-28 01:26:23 +0900

    mbti job popup

--- app/src/main/java/com/example/catbti/MbtiJobInfo.java
+++ app/src/main/java/com/example/catbti/MbtiJobInfo.java
@@ -51,8 +51,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ENFJ");
                 info_txt.setText("따뜻하고, 감정이입을 하며, 표현이 활발하고, 책임감이 있다. 비전과 목표에 대해서 사람들을 동기화시키는 리더십을 발휘한다.");
-                job_btn1.setText("CEO");
-                job_btn2.setText("언어교사");
+                job_btn1.setText("언어교사");
+                job_btn2.setText("CEO");
                 image_mbti.setImageResource(R.drawable.enfj);
 
             }
@@ -64,7 +64,7 @@ public class MbtiJobInfo extends AppCompatActivity {
                 title_mbti.setText("ENTP");
                 info_txt.setText("상황을 빠르게 이해하고, 활기차고, 거리낌 없이 표현한다. 관심의 폭이 넓고, 한 가지 새로운 흥미는 또 다른 것으로 바뀌기 쉽다.");
                 job_btn1.setText("영화감독");
-                job_btn2.setText("벤처 사업가");
+                job_btn2.setText("벤처사업가");
                 image_mbti.setImageResource(R.drawable.entp);
 
             }
@@ -76,7 +76,7 @@ public class MbtiJobInfo extends AppCompatActivity {
                 title_mbti.setText("ENTJ");
                 info_txt.setText("솔직하며, 결단력 있고, 타인들을 이끌고자 한다. 장기 계획과 목표를 설정하고 자신들의 아이디어와 비전을 뚜렷하게 표현하고 관철시킨다.");
                 job_btn1.setText("경영 컨설턴트");
-                job_btn2.setText("경제분석가");
+                job_btn2.setText("경제학자");
                 image_mbti.setImageResource(R.drawable.entj);
             }
         });
@@ -96,7 +96,7 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("INFJ");
                 info_txt.setText("다른 사람들에 대해 통찰력을 지니고 있다. 자신의 비전을 수행하기 위해 사람들을 조직화하고 동기화시킨다.");
-                job_btn1.setText("특수 교사");
+                job_btn1.setText("특수교사");
                 job_btn2.setText("아트디렉터");
                 image_mbti.setImageResource(R.drawable.infj);
             }
@@ -107,8 +107,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("INTJ");
                 info_txt.setText("독창적이고 창의적인 마인드를 지니고 있다. 회의적이고 독립적이며, 자신과 타인들에 대한 능력과 수행에 높은 기준을 지니고 있다.");
-                job_btn1.setText("웹 개발자");
-                job_btn2.setText("경영컨설턴트");
+                job_btn1.setText("연구원");
+                job_btn2.setText("경영 컨설턴트");
                 image_mbti.setImageResource(R.drawable.intj);
             }
         });
@@ -130,7 +130,7 @@ public class MbtiJobInfo extends AppCompatActivity {
                 title_mbti.setText("INFP");
                 info_txt.setText("호기심이 많고, 어떠한 일의 가능성을 보는 경향이 있다. 자신들의 가치가 위협받지 않는 한 잘 적응하고, 융통성이 있다.");
                 job_btn1.setText("작곡가");
-                job_btn2.setText("사서");
+                job_btn2.setText("작가");
                 image_mbti.setImageResource(R.drawable.infp);
             }
         });
@@ -140,8 +140,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ESFJ");
                 info_txt.setText("마음이 따뜻하고, 양심적이며, 협조적이다. 일상생활에서 타인들의 필요를 잘 알아패며, 그것을 제공하기 위해 노력한다.");
-                job_btn1.setText("간호사");
-                job_btn2.setText("초등학교 교사");
+                job_btn1.setText("승무원");
+                job_btn2.setText("특수 교사");
                 image_mbti.setImageResource(R.drawable.esfj);
             }
         });
@@ -151,8 +151,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ESTJ");
                 info_txt.setText("구체적이고, 현실적이며, 사실적이다. 결정을 하고자하고, 결정된 것들을 이행하기 위해 빠르게 움직인다.");
-                job_btn1.setText("예산분석가");
-                job_btn2.setText("교육전문가");
+                job_btn1.setText("판사");
+                job_btn2.setText("교사");
                 image_mbti.setImageResource(R.drawable.estj);
             }
         });
@@ -162,8 +162,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ESFP");
                 info_txt.setText("사교적이고, 다정하며, 수용적이며, 긍정적이다. 새로운 사람들과 환경에 빨리 적응한다.");
-                job_btn1.setText("의상디자이너");
-                job_btn2.setText("애니메이터");
+                job_btn1.setText("디자이너");
+                job_btn2.setText("빅데이터 전문가");
                 image_mbti.setImageResource(R.drawable.esfp);
             }
         });
@@ -173,8 +173,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ESTP");
                 info_txt.setText("상황에 유연하며, 문제해결을 위해 활동적으로 움직인다. 지금 벌어지는 일에 관심이 많고, 타인들과 활기차게 할 수 있는 매 순간을 즐긴다.");
-                job_btn1.setText("건축엔지니어");
-                job_btn2.setText("은행원");
+                job_btn1.setText("빅데이터 전문가");
+                job_btn2.setText("사업가");
                 image_mbti.setImageResource(R.drawable.estp);
             }
         });
@@ -185,7 +185,7 @@ public class MbtiJobInfo extends AppCompatActivity {
                 title_mbti.setText("ISFJ");
                 info_txt.setText("조용하고, 다정하며, 성실하고, 책임감이 강하다. 자신들의 의무에 헌신적이고, 이를 꾸준하게 실현해 나간다.");
                 job_btn1.setText("물리치료사");
-                job_btn2.setText("행정보조원");
+                job_btn2.setText("교사");
                 image_mbti.setImageResource(R.drawable.isfj);
             }
         });
@@ -195,8 +195,8 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ISTJ");
                 info_txt.setText("조용하고, 신중하며, 철저함과 확실성으로 좋은 결과를 얻고자 한다. 해야 할 것을 논리적으로 결정하고, 흐트러짐 없이 꾸준히 해 나간다.");
-                job_btn1.setText("통계학자");
-                job_btn2.setText("웹 개발자");
+                job_btn1.setText("교사");
+                job_btn2.setText("감독관");
                 image_mbti.setImageResource(R.drawable.istj);
             }
         });
@@ -207,7 +207,7 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ISFP");
                 info_txt.setText("조용하고, 다정하며, 정서에 민감하고, 친절하다. 논쟁과 갈등을 싫어하며, 자신의 의견이나 가치를 다른 사람들에게 강요하지 않는다.");
-                job_btn1.setText("만화가");
+                job_btn1.setText("물리치료사");
                 job_btn2.setText("음향디자이너");
                 image_mbti.setImageResource(R.drawable.isfp);
             }
@@ -218,7 +218,7 @@ public class MbtiJobInfo extends AppCompatActivity {
             public void onClick(View view) {
                 title_mbti.setText("ISTP");
                 info_txt.setText("상황에 대해 관조적이고 유연하다. 사실을 논리적으로 구조화하고자 하며, 효울성에 가치를 둔다.");
-                job_btn1.setText("운동선수");
+                job_btn1.setText("빅데이터 전문가");
                 job_btn2.setText("경제학자");
                 image_mbti.setImageResource(R.drawable.istp);
             }
--- app/src/main/java/com/example/catbti/mbti_job_info_popup.java
+++ app/src/main/java/com/example/catbti/mbti_job_info_popup.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class mbti_job_info_popup extends Activity {
@@ -19,6 +20,7 @@ public class mbti_job_info_popup extends Activity {
         TextView titleText = (TextView)findViewById(R.id.testpopTitleText);
         TextView jobInfoText = findViewById(R.id.testIText);
         Button closeBtn = findViewById(R.id.testPopupCloseBtn);
+        ImageView imageJobMBTI  = (ImageView)findViewById(R.id.jobMBTIImage);
 
         Intent intent = getIntent();
         String mbti = intent.getStringExtra("mbti");
@@ -26,11 +28,113 @@ public class mbti_job_info_popup extends Activity {
         titleText.setText(" "+mbti+"와 어울리는 직업 : "+jobName);
 
 
-        if(jobName.equals("통계학자")){
-            String teacher = " 통계학자의 특성은....";
+        if(jobName.equals("CEO")){
+            String CEO = " CEO의 특징은 리더쉽, 비전, 결단력입니다. 사람들을 동기화시키고 이끄는 것에 특출한 "+mbti+"의 여러분들은 뛰어난 리더쉽을 보유하여 사람들을 이끌어 갈 수 있고 이상적인 비전으로 미래로 이끌 힘을 가지고 있습니다. 마지막으로 확고한 결단력으로 여러분의 손익을 조절 할 능력을 가지고 있기 때문에 “CEO“라는 직업을 추천 드립니다.";
+            jobInfoText.setText(CEO);
+            imageJobMBTI.setImageResource(R.drawable.enfj_job);
+        }
+
+        else if(jobName.equals("언어교사")){
+            String language_teacher = " 언어교사의 특징은 표현력, 공감능력, 열정입니다. 누군가의 마음에 공감을 잘 해주는 "+mbti+"의 여러분들은 특출한 표현력으로 아이들에게 많은 영감을 심어줄 수 있고 뛰어난 공감능력으로 그들의 마음을 잘 헤아려 줄 수 있습니다. 마지막으로 누구보다 뛰어난 열정을 가지고 있기 때문에 “언어교사”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(language_teacher);
+            imageJobMBTI.setImageResource(R.drawable.enfj_job2);
+        }
+
+        else if(jobName.equals("영화감독")){
+            String movie = " 영화감독의 특징은 독창성, 카리스마, 객관성입니다. 넘치는 아이디어를 보유하고 게신 "+mbti+"의 여러분들은 독창적인 아이디어로 새로운 연출을 선보일 수 있고 날카로운 카리스마로 다른 이들을 통제 할 수 있으며 객관적인 시선으로 작품들을 더 뛰어나게 다듬을 수 있기 때문에 “영화감독”이라는 직업을 추천 드립니다.";
+            jobInfoText.setText(movie);
+        }
+
+        else if(jobName.equals("벤처사업가")){
+            String venture = " 벤처사업가의 특징은 사상가, 다식, 객관성입니다. 예리한 눈으로 미래를 지향할 수 있는 "+mbti+"의 여러분들은 아이디어를 유연하게 전환 할 수 있는 사상가 적인 생각을 보유 하고 있으며 다양한 지식을 보유 할 머리를 가지고 있습니다. 마지막으로 객관적인 판단으로 벤처기업들의 경영을 이끌어 나갈 힘을 가지고 있기 때문에 “벤처 사업가” 라는 직업을 추천 드립니다.";
+            jobInfoText.setText(venture);
+        }
+
+        else if(jobName.equals("경제학자")){
+            String economist = "경제학자의 특징은 분석력, 비판적, 개방적입니다. 엘리트 적인 면을 가지고 있는 "+mbti+"의 여러분 들은 뛰어난 분석력으로 사회의 경제 시장을 분석 할 수 있습니다. 또한 체계적인 비판으로 올바른 판단을 할 수 있고, 개방적인 마인드로 얻어낸 아이디어들을 경제학에 이용 할 수 있는 능력을 보유하고 있기 때문에 “경제학자”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(economist);
+            imageJobMBTI.setImageResource(R.drawable.entj_job1);
+        }
+
+        else if(jobName.equals("디자이너")){
+            String designer = " 디자이너의 특징은 관찰력, 표현력, 열정입니다. 방대한 양의 에너지를 보유한 "+mbti+"의 여러분들은 뛰어난 관찰력으로 당신의 시아속의 움직임을 놓치지 않고 잡아낼 수 있으며 잡아낸 움직임들을 당신의 표현력으로 새롭게 나타낼 수 있습니다. 또한 이 모든 걸 끊임없이 해낼 수 있는 열정을 가지고 있기 때문에 “디자이너”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(designer);
+        }
+
+        else if(jobName.equals("방송프로듀서")){
+            String broadcast = " 방송 프로듀서의 특징은 호기심, 활력, 커뮤니케이션입니다. 재기발랄 한 성향을 가지고 있는 "+mbti+"의 여러분들은 다양한 호기심으로 많은 것들로부터 영감을 얻어 당신의 기획력으로 바꿀 수 있습니다. 또한 넘치는 활력을 바탕으로 다른 동료들과 뛰어난 커뮤니케이션을 통해 방송을 제작해 나갈 수 있기 때문에 “방송 프로듀서”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(broadcast);
+        }
+
+        else if(jobName.equals("특수교사")){
+            String special = " 특수교사의 특징은 공감능력, 포옹력, 섬세함입니다. 선의의 마음을 가지고 있는 "+mbti+"의 여러분들은 뛰어난 공감능력으로 아이들의 아픔을 이해할 수 있습니다. 또한 넓은 포옹력으로 그들의 결함을 받아들이고 당신의 섬세함으로 그 결함들을 해결하는데 도움을 줄 수 있습니다. 때문에 “특수교사”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(special);
+        }
+
+        else if(jobName.equals("아트디렉터")){
+            String art = "아트디렉터의 특징은 예술성, 창의력, 표현력입니다. 뛰어난 미적 능력을 잠재하고 있는 "+mbti+"의 여러분들은 예술성을 가지고 있고 사람들을 놀라게 할 창의력을 가지고 있습니다. 뿐만 아니라 당신의 예술성과 창의력을 미적으로 표현해 낼 수 있는 표현력을 보유하고 있기 때문에 “아트디렉터”라는 직업을 추천 드립니다. ";
+            jobInfoText.setText(art);
+        }
+
+        else if(jobName.equals("연구원")){
+            String researcher = "  연구원의 특징은 호기심, 이론성, 목표 지향적입니다. 예상치 못할 성공을 가져올 힘을 가진 "+mbti+"의 여러분들은 깊은 호기심으로 알고자 하는 사실에 높은 학구열을 가지고 있습니다. 또한 이 호기심에 이론적인 면을 더하여 객관화 시킬 능력을 가지고 있습니다. 마지막으로 목표 지향적인 생각으로 하고자 한 일에 꾸준한 노력을 할 수 있기 때문에 “연구원”이라는 직업을 추천 드립니다.";
+            jobInfoText.setText(researcher);
+        }
+
+        else if(jobName.equals("경영 컨설턴트")){
+            String consultant = " 경영 컨설턴트의 특징은 유동성, 계획성, 호기심입니다. 뛰어난 통찰력을 가지고 있는 "+mbti+"의 여러분들은  시대의 변화에 따라 유동적으로 생각을 할 수 있고 높은 계획성으로 기업의 미래 경영을 만들어 나갈 수 있습니다. 또한 뛰어난 호기심으로 얻은 지식을 자신의 것으로 만들어 내 새로운 경영 방법을 생각해 낼 수 있는 능력을 가지고 있기 때문에 “경영 컨설턴트”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(consultant);
+        }
+
+        else if(jobName.equals("작곡가")){
+            String composer = " 작곡가의 특징은 창의성, 공감능력, 섬세함입니다. 크리에이터의 기질을 가진 "+mbti+"의 여러분들은 뛰어난 창의성으로 사람들에게 영감을 주는 가사를 만들어 낼 수 있습니다. 또한  가사 하나하나에 섬세한 표현으로, 많은 이들의 공감을 얻는 능력을 보유하고 있기 때문에 음악을 좋아하신다면 “작곡가”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(composer);
+        }
+
+        else if(jobName.equals("작가")){
+            String author = "작가의 특징은 몽상, 열성, 영감입니다. 다양한 재미있는 상상을 하는 것을 즐기는 "+mbti+"의 여러분 들은 다양한 몽상을 보유하고 있습니다. 또한 삶의 여러 요소에서 영감을 받아 이상적인 생각을 잘 하기 때문에 자신의 상상을 사람들에게 보여주고 싶다면 “작가”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(author);
+        }
+
+        else if(jobName.equals("승무원")){
+            String crew = " 항공기 승무원의 특징은 협력, 책임감, 배려심입니다. 누구에게나 밝게 웃어 줄 수 있는 "+mbti+"의 여러분들은 다른 이들과 협력하고자 하는 강한 마음을 가지고 있습니다. 또한 승객을 목적지 까지 안전하게 대려다 주겠다는 강한 책임감을 가지며, 항상 다른이들을 배려하는 마음을 보유하고 있기 때문에 “승무원”이라는 직업을 추천드립니다.";
+            jobInfoText.setText(crew);
+        }
+
+        else if(jobName.equals("판사")){
+            String judge = " 판사의 특징은 리더쉽 판단력 준법입니다. 항상 정의로움을 추구하는 "+mbti+"의 여러분들은 판결을 이끌어 갈 수 있는 리더쉽이 있고, 억울한 사람이 없도록 할 수 있는 판단력을 보유하고 있습니다. 또한 높은 기억력으로 판사의 역량을 충족시킬 수 있고 옳은 판결을 할 판단력을 가지고 있기 때문에 “판사”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(judge);
+        }
+
+        else if(jobName.equals("교사")){
+            String teacher = " 교사의 특징은 정직, 헌신, 리더쉽입니다. 다른 이들보다 더 성숙한 생각을 가지고 있는 "+mbti+"의 여러분들은 정의에 대한 높은 이해력으로 아직 미숙한 아이들에게 정직과 질서를 가르칠 수 있습니다. 또한 아이들을 올바른 길로 이끌 수 있는 리더쉽을 보유하고 있기 때문에  “교사”라는 직업을 추천 드립니다.";
             jobInfoText.setText(teacher);
         }
 
+        else if(jobName.equals("빅데이터 전문가")){
+            String bigdata = " 빅데이터 전문가의 특징은 관찰력, 통찰력입니다. 뛰어난 관찰력을 보유한 "+mbti+"의 여러분들은 빅데이터를 분석을 기반으로 한 시장의 미래 분석 등에 용의 주도 합니다. 또한 높은 통찰력으로 주어진 데이터를 이용한 올바른 미래 예견을 할 수 있기 때문에 “빅데이터 전문가”라는 직업을 추천 드립니다.\n";
+            jobInfoText.setText(bigdata);
+        }
+
+        else if(jobName.equals("물리치료사")){
+            String physical = " 물리치료사의 특징은 지원적, 신실성, 열정입니다. 든든한 사회의 수호자인 "+mbti+"의 여러분들은 항상 다른 이들을 돕고자 합니다. 또한 신실적이고 , 넘치는 열정을 보유하고 있기 때문에 신체적으로 장애가 있는 환자들 에게도 거리낌 없이 다가 갈 수 있는 성격을 보유 하고 있기 때문에 “물리치료사” 라는 직업을 추천 드립니다.";
+            jobInfoText.setText(physical);
+        }
+
+        else if(jobName.equals("사업가")){
+            String businessman = " 사업가의 특징은 사교성, 관찰력, 유동성입니다. 대담한 모험을 즐기는 "+mbti+"의 여러분들은 누구보다 뛰어난 사교성으로 다양한 사람들과 친해지는 데에 어려움을 겪지 않습니다. 또한 뛰어난 관찰력으로 사회 시장의 구조를 이해 할 수 있고 유동적인 생각으로 다양한 아이디어를 추구하기 때문에 “사업가”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(businessman);
+        }
+
+        else if(jobName.equals("감독관")){
+            String inspector = "감독관의 특징은 책임, 정직, 객관입니다. 청렴결백하고 정의로운 "+mbti+"의 여러분들은 책임감을 중요시 합니다. 정직한 규율에 따라 단체를 지휘하고, 감독할 수 있고. 그와 동시에 객관적으로 상황을 분석하여 일을 해결할 수 있는 능력을 보유하고 있습니다. 따라서 “감독관”이라는 직업을 추천합니다. ";
+            jobInfoText.setText(inspector);
+        }
+
+        else if(jobName.equals("음향디자이너")){
+            String sound = " 음디자이너의 특징은 관찰력, 표현력, 열정입니다. 예측할 수 없는 매력을 가진 "+mbti+"의 여러분들은 뛰어난 관찰력으로 당신의 시아속의 움직임을 놓치지 않고 잡아낼 수 있으며 잡아낸 움직임들을 당신의 표현력으로 새롭게 나타낼 수 있습니다. 또한 이 모든 걸 끊임없이 해낼 수 있는 열정을 가지고 있기 때문에 “음향 디자이너”라는 직업을 추천 드립니다.";
+            jobInfoText.setText(sound);
+        }
         closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
